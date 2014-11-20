2175b0642dd262ca1cb3104703d0974e96aacaf9
gayathri.307673 <budgetapptechm@users.noreply.github.com>
2014-11-20 05:23:39 +0000

    created on 20141120

---
+++ BudgetApp/src/com/gene/app/util/BudgetConstants.java
@@ -0,0 +1,62 @@
+package com.gene.app.util;
+
+public interface BudgetConstants {
+
+	//DBUtil.java
+	public static final String[] months = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN",
+		"JUL", "AUG", "SEP", "OCT", "NOV", "DEC", "TOTAL" };
+	public static final String total = "TOTAL";
+	public static final String seperator = " - ";
+	public static final String GTFReportFilterString = "key == keyParam";
+	public static final String GTFReportParameters = "String keyParam";
+	public static final String GTFReportOrderingParameters = "flag asc";
+	public static final String GTFReportOrderingParameters_getReport = "flag asc, projectName asc, gMemoryId asc";
+	
+	// This variable is used in UI layer and service layer
+	public static final String costCenter = "307673";
+	
+	//StoreReport.java
+	public static final String contentType = "text/plain";
+	public static final String objArray = "objarray";
+	public static final String loggedInUser = "loggedInUser";
+	public static final String status_New = "New";
+	public static final String status_Active = "Active";
+	public static final String GTFReport_ProjectWBS = "4";
+	public static final String GTFReport_ProjectName = "1";
+	public static final String GTFReport_Status = "2";
+	public static final String GTFReport_Requestor = "3";
+	public static final String GTFReport_WBS_Name = "5";
+	public static final String GTFReport_SubActivity = "6";
+	public static final String GTFReport_Brand = "7";
+	public static final String GTFReport_Percent_Allocation = "8";
+	public static final String GTFReport_PoNumber = "9";
+	public static final String GTFReport_PoDesc = "10";
+	public static final String GTFReport_Vendor = "11";
+	public static final String GTFReport_Remarks = "25";
+	public static final String REQUEST_ATTR_GTFReports = "gtfreports";
+	public static final String REQUEST_ATTR_SUMMARY = "summary";
+	public static final String GetReport_REDIRECTURL = "/listProjects";
+	public static final String USER_ROLE_PRJ_OWNER = "prjOwner";
+	public static final String KEY = "key";
+	public static final String CELL_VALUE = "cellValue";
+	public static final String CELL_NUM = "celNum";
+	public static final int CELL_REMARKS = 13;
+	public static final String isMultiBrand = "37";
+	public static final String multiBrandInput = "36";
+	
+	// New Store Report
+	
+	public static final String New_GTFReport_gMemoriId = "0";
+	public static final String New_GTFReport_ProjectName = "1";
+	public static final String New_GTFReport_ProjectOwner = "2";
+	public static final String New_GTFReport_Status = "New";
+	public static final String New_GTFReport_SubActivity = "5";
+	public static final String New_GTFReport_Brand = "6";
+	public static final String New_GTFReport_Percent_Allocation = "7";
+	public static final String New_GTFReport_PoNumber = "8";
+	public static final String New_GTFReport_PoDesc = "9";
+	public static final String New_GTFReport_Vendor = "10";
+	public static final String New_GTFReport_Remarks = "25";
+	public static final String New_isMultiBrand = "37";
+	public static final String New_multiBrandInput = "36";
+}
---
+++ BudgetApp/war/footer.jsp
@@ -0,0 +1,22 @@
+<!-- Add whatever you like to the footer -->
+<div style="height: 20px;">
+</div>
+<div id="footer" style="position: fixed; bottom: 0px; width: 100%; height: 20px; z-index: 2;">
+	<table width="100%">
+		<tbody>
+				<td height="20px" style="background-color: #005691; color: #FFFFFF;  font-size: 11px;"
+					width="87%">
+					<center>
+						Copyright Genentech, Inc. ©2014. <a href="/user-agreement/"
+							style="color: white; text-decoration: underline;">User
+							Agreement</a> <span>|</span> <a href="/privacy-policy/"
+							style="color: white; text-decoration: underline;">Privacy
+							Policy</a>
+					</center>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
