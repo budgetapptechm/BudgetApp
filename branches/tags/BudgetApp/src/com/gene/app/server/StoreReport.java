35613098391a25607471de1b8f6edb0f15940cda
gayathri.307673@gmail.com <gayathri.307673@gmail.com@80e4d81a-3405-206f-6867-bf47240bd553>
2014-11-21 06:47:36 +0000

    created on 20141121

---
+++ branches/tags/BudgetApp/src/com/gene/app/server/StoreReport.java
@@ -0,0 +1,434 @@
+package com.gene.app.server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gene.app.bean.GtfReport;
+import com.gene.app.bean.MultiBrand;
+import com.gene.app.bean.UserRoleInfo;
+import com.gene.app.util.BudgetConstants;
+import com.gene.app.util.DBUtil;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
+@SuppressWarnings("serial")
+public class StoreReport extends HttpServlet {
+	Map<String,Double> brandMap = new TreeMap<String,Double>();
+	DBUtil util = new DBUtil();
+	MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		String email = "";
+		HttpSession session = req.getSession();
+		User user = (User)session.getAttribute(BudgetConstants.loggedInUser);
+		email = user.getEmail();
+		resp.setContentType(BudgetConstants.contentType);
+		String objarray = req.getParameter(BudgetConstants.objArray).toString();
+		storeProjectData(objarray, email);
+		/*boolean isMultiBrand = Boolean.parseBoolean(req.getParameter("multibrand").toString());
+		String email = "";
+		HttpSession session = req.getSession();
+		User user = (User)session.getAttribute(BudgetConstants.loggedInUser);
+		email = user.getEmail();
+		String brand = "";
+		Map<String,MultiBrand> multiBrandMap = prepareMultiBrandMap();
+		if(isMultiBrand){
+			storeMultiBrandProject(objarray, email,multiBrandMap);
+		}else{
+			storeSingleBrandProject(objarray, email);
+		}
+		brandMap.put(brand, 60000.0);*/
+		//insertUserRoleInfo((User)session.getAttribute(BudgetConstants.loggedInUser));
+	}
+	
+	
+	public void storeProjectData(String objarray, String email){
+		List<GtfReport> gtfReports = new ArrayList<GtfReport>();
+		JSONArray jsonArray = null;
+		GtfReport gtfReport = null;
+		JSONObject rprtObject = null;
+		String projectWBS = "";
+		String remarks = null;
+		String multiBrand = "";
+		try{
+			jsonArray = new JSONArray(objarray);
+			for (int count = 0; count < jsonArray.length(); count++) {
+				gtfReport = new GtfReport();
+				rprtObject = jsonArray.getJSONObject(count);
+				/*projectWBS = rprtObject.getString(BudgetConstants.New_GTFReport_ProjectWBS);
+				if(projectWBS == null || projectWBS.isEmpty() || projectWBS.length() == 0){
+					continue;
+				}*/
+				gtfReport.setEmail(email);
+				String status =BudgetConstants.New_GTFReport_Status;
+				int flag = 0;
+				if(BudgetConstants.status_New.equalsIgnoreCase(status.trim())){
+					flag = 1;
+				}else if(BudgetConstants.status_Active.equalsIgnoreCase(status.trim())){
+					flag = 2;
+				}else{
+					flag = 3;
+				}
+				gtfReport.setFlag(flag);
+				gtfReport.setStatus(status);
+				//gtfReport.setStatus(rprtObject.getString("2"));
+				
+				gtfReport.setRequestor(rprtObject.getString(BudgetConstants.New_GTFReport_ProjectOwner));
+				//gtfReport.setProject_WBS(rprtObject.getString(BudgetConstants.New_GTFReport_ProjectWBS));
+				//gtfReport.setWBS_Name(rprtObject.getString(BudgetConstants.New_GTFReport_WBS_Name));
+				gtfReport.setSubActivity(rprtObject.getString(BudgetConstants.New_GTFReport_SubActivity));
+				gtfReport.setPoNumber(rprtObject.getString(BudgetConstants.New_GTFReport_PoNumber));
+				String poDesc = rprtObject.getString(BudgetConstants.New_GTFReport_PoDesc);
+				gtfReport.setPoDesc(poDesc);
+				gtfReport.setVendor(rprtObject.getString(BudgetConstants.New_GTFReport_Vendor));
+				
+				try{
+					remarks = ((rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)!=null) && (!"".equalsIgnoreCase(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks).trim())))?(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)):"";
+				}catch(com.google.appengine.labs.repackaged.org.json.JSONException exception){
+					remarks = "";
+				}
+				gtfReport.setRemarks(remarks);
+				multiBrand = rprtObject.getString(BudgetConstants.isMultiBrand);
+				if(multiBrand !=null && !"".equalsIgnoreCase(multiBrand.trim()) && "true".equalsIgnoreCase(multiBrand.trim())){
+					prepareMultiBrandProjectData(gtfReports,gtfReport,rprtObject);
+				}else{
+					prepareSingleBrandProjectData(gtfReports,gtfReport,rprtObject);
+				}
+				//gtfReports.add(gtfReport);
+			}
+			util.generateProjectIdUsingJDOTxn(gtfReports);
+			util.storeProjectsToCache(gtfReports);
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	public void prepareSingleBrandProjectData(List<GtfReport> gtfReports,GtfReport gtfReport,JSONObject rprtObject){
+		try{
+		
+			gtfReport.setgMemoryId(rprtObject.getString(BudgetConstants.New_GTFReport_gMemoriId));
+		gtfReport.setProjectName(rprtObject.getString(BudgetConstants.New_GTFReport_ProjectName));
+		gtfReport.setBrand(rprtObject.getString(BudgetConstants.New_GTFReport_Brand));
+		try {
+			gtfReport.setPercent_Allocation(Integer.parseInt(rprtObject
+					.getString(BudgetConstants.New_GTFReport_Percent_Allocation)));
+		} catch (NumberFormatException e) {
+			gtfReport.setPercent_Allocation(0);
+		}
+		//String poDesc = rprtObject.getString(BudgetConstants.New_GTFReport_PoDesc);
+	//	gtfReport.setgMemoryId(poDesc.substring(0, 6));
+		Map<String, Double> plannedMap = new HashMap<String, Double>();
+		Map<String, Double> setZeroMap = new HashMap<String, Double>();
+		for (int cnt = 0; cnt <= BudgetConstants.months.length-1; cnt++) {
+			setZeroMap.put(BudgetConstants.months[cnt], 0.0);
+			try {
+				plannedMap.put(BudgetConstants.months[cnt],
+						Double.parseDouble(rprtObject.getString(Integer.toString(cnt+BudgetConstants.months.length-1))));
+			} catch (NumberFormatException e ) {
+				plannedMap.put(BudgetConstants.months[cnt], 0.0);
+			}
+		}
+		gtfReport.setPlannedMap(plannedMap);
+		gtfReport.setBenchmarkMap(setZeroMap);
+		gtfReport.setAccrualsMap(setZeroMap);
+		gtfReport.setVariancesMap(setZeroMap);
+		gtfReport.setMultiBrand(false);
+		gtfReports.add(gtfReport);
+		
+		}catch(JSONException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void prepareMultiBrandProjectData(List<GtfReport> gtfReports,GtfReport gtfReport1,JSONObject rprtObject){
+		prepareSingleBrandProjectData(gtfReports, gtfReport1, rprtObject);
+		GtfReport gtfReport = null; 
+		JSONArray jsonArray = null;
+		JSONObject multiBrandObject = null;
+		Double value = 0.0;
+		double percent_allocation = 0.0;
+		try{
+		String mutliBrandArray = rprtObject.getString(BudgetConstants.multiBrandInput);
+		jsonArray = new JSONArray(mutliBrandArray);
+		for(int i=0;i<jsonArray.length();i++){
+			try {
+				gtfReport = (GtfReport)gtfReport1.clone();
+			} catch (CloneNotSupportedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			multiBrandObject = jsonArray.getJSONObject(i);
+			if(multiBrandObject.getString("4") ==null || "".equals(multiBrandObject.getString("4").trim())){
+				break;
+			}
+		gtfReport.setProjectName(multiBrandObject.getString("4"));
+		gtfReport.setBrand(multiBrandObject.getString("1"));
+		gtfReport.setgMemoryId(multiBrandObject.getString("5"));
+		try {
+			gtfReport.setPercent_Allocation(Double.parseDouble(multiBrandObject
+					.getString("2")));
+		} catch (NumberFormatException e) {
+			gtfReport.setPercent_Allocation(0);
+		}
+		percent_allocation = Double.parseDouble(multiBrandObject.getString("2"));
+		gtfReport.setgMemoryId(multiBrandObject.getString("5"));
+		Map<String, Double> plannedMap = new HashMap<String, Double>();
+		Map<String, Double> setZeroMap = new HashMap<String, Double>();
+		Map<String,Double> parentPlannedMap = gtfReport.getPlannedMap();
+		for (int cnt = 0; cnt <= BudgetConstants.months.length-1; cnt++) {
+			setZeroMap.put(BudgetConstants.months[cnt], 0.0);
+			try {
+				value = parentPlannedMap.get(BudgetConstants.months[cnt])*percent_allocation/100;
+				plannedMap.put(BudgetConstants.months[cnt],value);
+			} catch (NumberFormatException e ) {
+				plannedMap.put(BudgetConstants.months[cnt], 0.0);
+			}
+		}
+		gtfReport.setPlannedMap(plannedMap);
+		gtfReport.setBenchmarkMap(setZeroMap);
+		gtfReport.setAccrualsMap(setZeroMap);
+		gtfReport.setVariancesMap(setZeroMap);
+		gtfReport.setMultiBrand(true);
+		gtfReports.add(gtfReport);
+		}
+		}catch(JSONException e){
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Store single brand project.
+	 *
+	 * @param objarray the objarray
+	 * @param email the email
+	 */
+	/*public void storeSingleBrandProject(String objarray, String email){
+		List<GtfReport> gtfReports = new ArrayList<GtfReport>();
+		try {
+			JSONArray jsonArray = new JSONArray(objarray);
+			for (int count = 0; count < jsonArray.length(); count++) {
+				GtfReport gtfReport = new GtfReport();
+				JSONObject rprtObject = jsonArray.getJSONObject(count);
+				String projectWBS = rprtObject.getString(BudgetConstants.New_GTFReport_ProjectWBS);
+				if(projectWBS == null || projectWBS.isEmpty() || projectWBS.length() == 0){
+					continue;
+				}
+				gtfReport.setProjectName(rprtObject.getString(BudgetConstants.New_GTFReport_ProjectName));
+				gtfReport.setEmail(email);
+				String status = rprtObject.getString(BudgetConstants.New_GTFReport_Status);
+				int flag = 0;
+				if(BudgetConstants.status_New.equalsIgnoreCase(status.trim())){
+					flag = 1;
+				}else if(BudgetConstants.status_Active.equalsIgnoreCase(status.trim())){
+					flag = 2;
+				}else{
+					flag = 3;
+				}
+				gtfReport.setFlag(flag);
+				gtfReport.setStatus(status);
+				//gtfReport.setStatus(rprtObject.getString("2"));
+				gtfReport.setRequestor(rprtObject.getString(BudgetConstants.New_GTFReport_Requestor));
+				gtfReport.setProject_WBS(rprtObject.getString(BudgetConstants.New_GTFReport_ProjectWBS));
+				gtfReport.setWBS_Name(rprtObject.getString(BudgetConstants.New_GTFReport_WBS_Name));
+				gtfReport.setSubActivity(rprtObject.getString(BudgetConstants.New_GTFReport_SubActivity));
+				gtfReport.setBrand(rprtObject.getString(BudgetConstants.New_GTFReport_Brand));
+				String brand = rprtObject.getString(BudgetConstants.New_GTFReport_Brand);
+				try {
+					gtfReport.setPercent_Allocation(Integer.parseInt(rprtObject
+							.getString(BudgetConstants.New_GTFReport_Percent_Allocation)));
+				} catch (NumberFormatException e) {
+					gtfReport.setPercent_Allocation(0);
+				}
+				gtfReport.setPoNumber(rprtObject.getString(BudgetConstants.New_GTFReport_PoNumber));
+				String poDesc = rprtObject.getString(BudgetConstants.New_GTFReport_PoDesc);
+				gtfReport.setgMemoryId(poDesc.substring(0, 6));
+				gtfReport.setPoDesc(poDesc.substring(7, poDesc.length()));
+				gtfReport.setVendor(rprtObject.getString(BudgetConstants.New_GTFReport_Vendor));
+				String remarks = null;
+				try{
+					remarks = ((rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)!=null) && (!"".equalsIgnoreCase(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks).trim())))?(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)):"";
+				}catch(com.google.appengine.labs.repackaged.org.json.JSONException exception){
+					remarks = "";
+				}
+				gtfReport.setRemarks(remarks);
+				Map<String, Double> benchmarkMap = new HashMap<String, Double>();
+				Map<String, Double> setZeroMap = new HashMap<String, Double>();
+				for (int cnt = 0; cnt <= BudgetConstants.months.length-1; cnt++) {
+					setZeroMap.put(BudgetConstants.months[cnt], 0.0);
+					try {
+						benchmarkMap.put(BudgetConstants.months[cnt],
+								Double.parseDouble(rprtObject.getString(Integer.toString(cnt+BudgetConstants.months.length-1))));
+					} catch (NumberFormatException e ) {
+						benchmarkMap.put(BudgetConstants.months[cnt], 0.0);
+					}
+				}
+				gtfReport.setBenchmarkMap(benchmarkMap);
+				gtfReport.setPlannedMap(setZeroMap);
+				gtfReport.setAccrualsMap(setZeroMap);
+				gtfReport.setVariancesMap(setZeroMap);
+				gtfReport.setMultiBrand(false);
+				gtfReports.add(gtfReport);
+				
+			}
+
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		util.generateProjectIdUsingJDOTxn(gtfReports);
+		util.storeProjectsToCache(gtfReports);
+	}
+	
+	*//**
+	 * Store multi brand project.
+	 *
+	 * @param objarray the objarray
+	 * @param email the email
+	 *//*
+	public void storeMultiBrandProject(String objarray, String email,Map<String,MultiBrand> multiBrandMap){
+		List<GtfReport> gtfReports = new ArrayList<GtfReport>();
+		boolean isFirstLoop = true;
+		try {
+			JSONArray jsonArray = new JSONArray(objarray);
+			String requestor = "";
+			String projectName = "";
+			String status = "";
+			int flag = 0;
+			String subActivity = "";
+			String poNumber = "";
+			String vendor = "";
+			String poDesc = "";
+			String projectWBS = "";
+			String WBSName = "";
+			
+			for (int count = 0; count < jsonArray.length(); count++) {
+				GtfReport gtfReport = new GtfReport();
+				JSONObject rprtObject = jsonArray.getJSONObject(count);
+				if(isFirstLoop){
+					isFirstLoop = false;
+					requestor = rprtObject.getString(BudgetConstants.New_GTFReport_Requestor);
+					projectName = rprtObject.getString(BudgetConstants.New_GTFReport_ProjectName);
+					status = rprtObject.getString(BudgetConstants.New_GTFReport_Status);
+					if(BudgetConstants.status_New.equalsIgnoreCase(status.trim())){
+						flag = 1;
+					}else if(BudgetConstants.status_Active.equalsIgnoreCase(status.trim())){
+						flag = 2;
+					}else{
+						flag = 3;
+					}
+					subActivity = rprtObject.getString(BudgetConstants.New_GTFReport_SubActivity);
+					poNumber = rprtObject.getString(BudgetConstants.New_GTFReport_PoNumber);
+					vendor = rprtObject.getString(BudgetConstants.New_GTFReport_Vendor);
+					poDesc = rprtObject.getString(BudgetConstants.New_GTFReport_PoDesc);
+					projectWBS = rprtObject.getString(BudgetConstants.New_GTFReport_ProjectWBS);
+					WBSName = rprtObject.getString(BudgetConstants.New_GTFReport_WBS_Name);
+				}
+
+				gtfReport.setRequestor(requestor);
+				gtfReport.setProjectName(projectName);
+				gtfReport.setEmail(email);
+				String brand = rprtObject.getString(BudgetConstants.New_GTFReport_Brand);
+				if(brand == null || brand.isEmpty() || brand.length() == 0){
+					continue;
+				}
+				gtfReport.setFlag(flag);
+				gtfReport.setStatus(status);
+				gtfReport.setProject_WBS(projectWBS);
+				gtfReport.setWBS_Name(WBSName);
+				gtfReport.setSubActivity(subActivity);
+				gtfReport.setBrand(brand);
+				try {
+					gtfReport.setPercent_Allocation(Double.parseDouble(rprtObject
+							.getString(BudgetConstants.New_GTFReport_Percent_Allocation)));
+				} catch (NumberFormatException e) {
+					gtfReport.setPercent_Allocation(0);
+				}
+				gtfReport.setPoNumber(poNumber);
+				if (count != 0) {
+					gtfReport
+							.setgMemoryId(poDesc.substring(0, 6) + "." + count);
+				}else{
+					gtfReport
+					.setgMemoryId(poDesc.substring(0, 6));
+				}
+				gtfReport.setPoDesc(poDesc.substring(7, poDesc.length()));
+				gtfReport.setVendor(vendor);
+				String remarks = null;
+				try{
+					remarks = ((rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)!=null) && (!"".equalsIgnoreCase(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks).trim())))?(rprtObject.getString(BudgetConstants.New_GTFReport_Remarks)):"";
+				}catch(com.google.appengine.labs.repackaged.org.json.JSONException exception){
+					remarks = "";
+				}
+				gtfReport.setRemarks(remarks);
+				Map<String, Double> benchmarkMap = new HashMap<String, Double>();
+				Map<String, Double> setZeroMap = new HashMap<String, Double>();
+				for (int cnt = 0; cnt <= BudgetConstants.months.length-1; cnt++) {
+					setZeroMap.put(BudgetConstants.months[cnt], 0.0);
+					try {
+						benchmarkMap.put(BudgetConstants.months[cnt],
+								Double.parseDouble(rprtObject.getString(Integer.toString(cnt+BudgetConstants.months.length-1))));
+					} catch (NumberFormatException e ) {
+						benchmarkMap.put(BudgetConstants.months[cnt], 0.0);
+					}
+				}
+				gtfReport.setBenchmarkMap(benchmarkMap);
+				gtfReport.setPlannedMap(setZeroMap);
+				gtfReport.setAccrualsMap(setZeroMap);
+				gtfReport.setVariancesMap(setZeroMap);
+				gtfReport.setMultiBrand(true);
+				gtfReports.add(gtfReport);
+			}
+
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
+		util.generateProjectIdUsingJDOTxn(gtfReports);
+		util.storeProjectsToCache(gtfReports);
+		//cache.delete(BudgetConstants.costCenter);
+	}*/
+
+	/**
+	 * Generate project id using jdo txn.
+	 *
+	 * @param gtfReports the gtf reports
+	 */
+	
+
+	
+	/**
+	 * Require to manually insert user role info in to datastore.
+	 * Required at the initiation of a new application.
+	 *
+	 * @param user the user
+	 */
+	public void insertUserRoleInfo(User user){
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		UserRoleInfo userInfo = new UserRoleInfo();
+		userInfo.setEmail(user.getEmail());
+		userInfo.setBrand(brandMap);
+		userInfo.setUserName(user.getNickname());
+		userInfo.setRole("Project Owner");
+		
+		try{
+			pm.makePersistent(userInfo);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			pm.close();
+		}
+	}
+
+}
---
+++ branches/tags/BudgetApp/war/header.jsp
@@ -0,0 +1,118 @@
+<%@page import="com.gene.app.bean.UserRoleInfo"%>
+<%@ page import="java.security.Principal"%>
+<%@ page import="com.google.appengine.api.users.UserService"%>
+<%@ page import="com.google.appengine.api.users.UserServiceFactory"%>
+<%@ page import="com.google.appengine.api.users.User"%>
+<%@ page import="java.util.logging.*"%>
+<%@ page import="com.google.appengine.api.memcache.*"%>
+<%@ page import="com.gene.app.util.*" %>
+
+<head>
+<title>Budget App</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/menustyles.css">
+<script src="SlickGrid-master/lib/jquery-1.7.min.js"
+	type="text/javascript"></script>
+<script src="scripts/menuscript.js"></script>
+</head>
+<html>
+
+<body bgcolor="#F4F6F8">
+	<%!private static Logger log = Logger.getLogger("header.jsp");%>
+
+	<%
+		UserService userService = UserServiceFactory.getUserService();
+		String requestUri = request.getRequestURI();
+		Principal userPrincipal = request.getUserPrincipal();
+		String loginLink = userService.createLoginURL(requestUri);
+		String email = userService.getCurrentUser().getEmail();
+		request.setAttribute("email", email);
+		Boolean isAdmin = false;
+		String userName = "";
+		String logoutLink = "";
+		DBUtil util = new DBUtil();
+		String costCenter="307673";
+		/*  UserDataUtil util1 = new UserDataUtil();
+		util1.insertCCMapping();
+		util1.insertUserRoleInfo();
+		util1.insertBudgetSummary(); */
+		if (userPrincipal != null) {
+			logoutLink = userService.createLogoutURL(requestUri);
+			User user = userService.getCurrentUser();
+			session.setAttribute("loggedInUser",user);
+			isAdmin = userService.isUserAdmin();
+			
+			boolean isGeneUser = false;//util.readUserRoleInfo(email,costCenter);
+			UserRoleInfo userInfo = util.readUserRoleInfo(email, costCenter);
+			String role = "";
+			if(userInfo!=null){
+				session.setAttribute("userInfo", userInfo);
+				if(email.equalsIgnoreCase(userInfo.getEmail())){
+					isGeneUser = true;
+					role = userInfo.getRole();
+				}
+			}
+			//Set username message
+		
+			/* if (isAdmin && isGeneUser) {
+				userName = "Welcome, " + user.getNickname() + role +" !";
+			} else if(!isAdmin) {
+				userName = "Welcome, " + user.getNickname() + "!";
+			} */
+			if (userService!= null && userService.isUserLoggedIn()    /* && isGeneUser   */) {
+				userName = "Welcome, " + user.getNickname() + " (" + role +") !";
+			} else{%>
+				alert("You are not authorised to access the application !!");
+			<%	response.sendRedirect(userService.createLogoutURL(requestUri));
+			} 
+	%>
+
+	<table style="width: 100%;">
+		<tr>
+			<td style="width: 25%; float: left"><a id="logo-container"
+				href="/"> <img src="images/logo.png"
+					alt="Genentech - A Member of the Roche Group" width="120"
+					height="32" class="gene-logo has-high-res">
+			</a></td>
+
+			<td style="width: 50%; align: center">
+				<div style="text-align: center; vertical-align: middle;">
+					<h1
+						style="color: #105596; font-family: 'trebuchet ms'; font-size: 25px; letter-spacing: 5px; padding-top: 8px;">
+						g<span style="font-variant: small-caps;">Memori Budgeting
+							Tool</span>
+					</h1>
+
+				</div>
+			</td>
+			<td style="width: 25%; float: right">
+				<div style="text-align: right;">
+					<span
+						style="color: #105596; font-family: 'trebuchet ms'; font-size: 14px;"><i><%=userName%></i>
+					</span>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td colspan="5" style="background-color: #e1e2e2; width: 100%"></td>
+		</tr>
+	</table>
+	<div style="height: 5px;"></div>
+	<div id='cssmenu'>
+		<ul>
+			<li><a href='/'><span>Home</span></a></li>
+			<li class='last'><a href='/createProjects'><span>New Projects</span></a></li>
+			<li class='last'><a href='/displayReports'><span>Reports</span></a></li>
+			<%
+				if (isAdmin) {
+			%>
+			<li class='last'><a href='/developer'><span>Admin</span></a></li>
+			<%
+				}
+			%>
+			<li class='last'><a href=<%=logoutLink%>><span>Logout</span></a></li>
+		</ul>
+	</div>
+	<%
+		}
+	%>
