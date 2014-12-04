f8b835991a458cb7c4c7b05e97f4569f8625e22a
gayathri.307673@gmail.com <gayathri.307673@gmail.com@80e4d81a-3405-206f-6867-bf47240bd553>
2014-12-04 06:28:23 +0000

    created on 20141204

---
+++ branches/tags/BudgetApp/src/com/gene/app/server/GetReport.java
@@ -0,0 +1,97 @@
+package com.gene.app.server;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.gene.app.bean.BudgetSummary;
+import com.gene.app.bean.GtfReport;
+import com.gene.app.bean.UserRoleInfo;
+import com.gene.app.util.BudgetConstants;
+import com.gene.app.util.DBUtil;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
+public class GetReport extends HttpServlet {
+
+	MemcacheService cache = MemcacheServiceFactory.getMemcacheService();
+	DBUtil util = new DBUtil();
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		HttpSession session = req.getSession();
+		resp.setContentType(BudgetConstants.contentType);
+		UserRoleInfo user = (UserRoleInfo)session.getAttribute("userInfo");
+		UserService userService;
+		String email ="";
+		Map<String,GtfReport> gtfReports = new LinkedHashMap<String,GtfReport>();
+		
+		if(user==null){
+		userService = UserServiceFactory.getUserService();//(User)session.getAttribute("loggedInUser");
+		email = userService.getCurrentUser().getEmail();
+		user = util.readUserRoleInfo(email);
+		}else{
+		email = user.getEmail();
+		}
+		gtfReports = util.getAllReportDataFromCache(user.getCostCenter());
+		
+		List<GtfReport> gtfReportList = getReportList(gtfReports,BudgetConstants.USER_ROLE_PRJ_OWNER,email);
+		Collections.sort( gtfReportList, new Comparator<GtfReport>()
+		        {
+		            public int compare( GtfReport o1, GtfReport o2 )
+		            {
+		            	if(o1.getFlag() == o2.getFlag()) {
+		            		if((o1.getProjectName()).compareTo(o2.getProjectName()) ==0){
+		            			return (o1.getgMemoryId()).compareTo(o2.getgMemoryId());
+		            		}
+		            		return (o1.getProjectName()).compareTo(o2.getProjectName());
+		            	}
+		            	return o1.getFlag() - o2.getFlag();
+		            }
+		        } );
+		gtfReportList = util.calculateVarianceMap(gtfReportList);
+		req.setAttribute(BudgetConstants.REQUEST_ATTR_GTFReports, gtfReportList);
+		DBUtil util = new DBUtil();
+		//UserRoleInfo user = util.readUserRoleInfo(email);
+		//BudgetSummary summary = util.readBudgetSummary(email,BudgetConstants.costCenter,gtfReportList,user);
+		BudgetSummary summary = util.readBudgetSummary(user.getCostCenter());
+		req.setAttribute("user", user);
+		session.setAttribute(BudgetConstants.REQUEST_ATTR_SUMMARY, summary);
+		RequestDispatcher rd = req.getRequestDispatcher(BudgetConstants.GetReport_REDIRECTURL);
+		try {
+			rd.forward(req, resp);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public List<GtfReport> getReportList(Map<String,GtfReport>gtfReports,String userType,String email){
+		List<GtfReport> gtfReportList = new ArrayList<GtfReport>();
+		GtfReport gtfReport = null;
+		
+		if(gtfReports!=null){
+			
+			for(Map.Entry<String, GtfReport> gtfEntry:gtfReports.entrySet()){
+				gtfReport = gtfEntry.getValue();
+				if((email !=null && !"".equals(email.trim())) && (gtfReport.getEmail().trim().toLowerCase()).contains(email.toLowerCase())){
+				gtfReportList.add(gtfReport);
+			}}
+		}
+		return gtfReportList;
+	}
+}
---
+++ branches/tags/BudgetApp/src/com/gene/app/util/UserDataUtil.java
@@ -0,0 +1,112 @@
+package com.gene.app.util;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.jdo.PersistenceManager;
+
+import com.gene.app.bean.BudgetSummary;
+import com.gene.app.bean.CostCenter_Brand;
+import com.gene.app.bean.UserRoleInfo;
+import com.gene.app.server.PMF;
+import com.google.apphosting.api.DatastorePb.Cost;
+
+public class UserDataUtil {
+	
+	// create username array
+	//String [] userName = {"mathura2","nellurks","siddagov","goldy","kaviv","sreedhac","makodea","singhb15","chinthb2"};//,"","","",""};
+	String [] userName = {"mathura2","nellurks","siddagov","goldy","gayathri","sreedhac","makodea","baba","chinthb2","test"};//,"","","",""};
+	// create user email array
+	String [] userEmail = {"mathura2@gene.com","nellurks@gene.com","siddagov@gene.com","goldy@gene.com","gayathri.307673@gmail.com","sreedhac@gene.com",
+			"makodea@gene.com","babajyoti.prakash10@gmail.com","chinthb2@gmail.com","test@example.com"};//,"","","",""};
+	//String [] userEmail = {"test@example.com"};
+	// create brandmap array
+
+	// create role array
+	String [] role = {"Project Owner","Project Owner","Brand Owner","Project Owner","Project Owner","Project Owner","Project Owner","Project Owner","Project Owner","Project Owner"};
+	//String [] role = {"Project Owner"};
+	String [] costCenter = {"307673","235031","307672","235032","307674","307675","307676","307677","307678","307680"};
+	public void insertUserRoleInfo(){
+		Map<String,Double> brandMap = new LinkedHashMap<String,Double>();
+		brandMap.put("Avastin",  60000.0);
+		brandMap.put("Onart", 30000.0);
+		brandMap.put("Perjeta", 20000.0);
+		brandMap.put("Tarceva", 50000.0);
+		
+	PersistenceManager pm = PMF.get().getPersistenceManager();
+	List<UserRoleInfo> userInfoList = new ArrayList<UserRoleInfo>();
+	UserRoleInfo userRoleInfo = null;
+	for(int i=0;i<userName.length;i++){
+	userRoleInfo = new UserRoleInfo();
+	userRoleInfo.setEmail(userEmail[i]);
+	userRoleInfo.setBrand(brandMap);
+	userRoleInfo.setUserName(userName[i]);
+	userRoleInfo.setRole(role[i]);
+	userRoleInfo.setCostCenter(costCenter[i]);
+	userInfoList.add(userRoleInfo);
+	}
+	try{
+		pm.makePersistentAll(userInfoList);
+	}catch(Exception e){
+		e.printStackTrace();
+	}finally{
+		pm.close();
+	}
+}
+	
+	public void insertBudgetSummary(){
+		Double[] budgetArray = {7000.0,10000.0,30000.0,60000.0,6000.0,2000.0,7000.0,30000.0,6000.0};
+		//Double[] budgetArray = {7000.0};
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		List<BudgetSummary> budgetSummaryList = new ArrayList<BudgetSummary>();
+		BudgetSummary budgetSummary = null;
+		for(int i=0;i<budgetArray.length;i++){
+		budgetSummary = new BudgetSummary();
+		budgetSummary.setProjectOwnerEmail(userEmail[i]);
+		budgetSummary.setTotalBudget(budgetArray[i]);
+		budgetSummaryList.add(budgetSummary);
+		}
+		try{
+			pm.makePersistentAll(budgetSummaryList);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			pm.close();
+		}
+	}
+	
+	
+	
+	public void insertCCMapping(){
+		Double[] budgetArray = {7000.0,10000.0,30000.0,60000.0,6000.0,2000.0,7000.0,30000.0,6000.0};
+		//Double[] budgetArray = {7000.0};
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		CostCenter_Brand cc = new CostCenter_Brand();
+		//List<BudgetSummary> budgetSummaryList = new ArrayList<BudgetSummary>();
+		/*BudgetSummary budgetSummary = null;
+		for(int i=0;i<budgetArray.length;i++){
+		budgetSummary = new BudgetSummary();
+		budgetSummary.setProjectOwnerEmail(userEmail[i]);
+		budgetSummary.setTotalBudget(budgetArray[i]);
+		budgetSummaryList.add(budgetSummary);
+		}*/
+		List<CostCenter_Brand> ccList = new ArrayList<CostCenter_Brand>();
+		for(int i=0;i<userName.length;i++){
+			cc = new CostCenter_Brand();
+			cc.setBrandFromDB("Perjeta:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=30000.0;Avastin:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=40000.0;Tarceva:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=50000.0;Onart:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=60000.0;");
+			cc.setCostCenter(costCenter[i]);
+			ccList.add(cc);
+		}
+		//cc.setBrandFromDB("Perjeta:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=30000.0;Avastin:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=40000.0;Tarceva:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=50000.0;Onart:planned=0.0:accrual=0.0:benchMark=0.0:variance=0.0:total=60000.0;");
+		//cc.setCostCenter("307673");
+		try{
+			pm.makePersistentAll(ccList);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			pm.close();
+		}
+	}
+}
