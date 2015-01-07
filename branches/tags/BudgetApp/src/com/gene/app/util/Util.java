ca9bc1471d202b3810db431fa3cb45c1d2871ebe
babajyoti.prakash10@gmail.com <babajyoti.prakash10@gmail.com@80e4d81a-3405-206f-6867-bf47240bd553>
2015-01-07 07:12:56 +0000

    version on 20145 01 07

---
+++ branches/tags/BudgetApp/src/com/gene/app/util/Util.java
@@ -0,0 +1,33 @@
+package com.gene.app.util;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Util {
+	
+	/**
+	 * To round up a double value.
+	 *
+	 * @param Value the value to be rounded up
+	 * @param Rounder up to how many decimal places
+	 * @return Rounded up double value
+	 */
+	public static double roundDoubleValue(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    BigDecimal bd = new BigDecimal(value);
+	    bd = bd.setScale(places, RoundingMode.HALF_UP);
+	    return bd.doubleValue();
+	}
+	
+	/**
+	 * Checks if it is an int value.
+	 *
+	 * @param value the value
+	 * @return true, if is an int value
+	 */
+	public static boolean isInt(String value){
+		return value.matches("[-+]?[0-9]+");
+	}
+	
+}
