771acc27674d7c1a43bd5c924ddeb0f0bfad9923
vijaykanthgoud <budgetapptechm@users.noreply.github.com>
2015-04-06 11:34:18 +0000

    Qtr cut off dates

---
+++ BudgetApp/src/com/gene/app/model/QTR_CUTOFF_DATES.java
@@ -0,0 +1,22 @@
+package com.gene.app.model;
+
+import java.util.Date;
+
+public class QTR_CUTOFF_DATES {
+
+	private String qtr;
+	private Date dateTime;
+	
+	public String getQtr() {
+		return qtr;
+	}
+	public void setQtr(String qtr) {
+		this.qtr = qtr;
+	}
+	public Date getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+}
