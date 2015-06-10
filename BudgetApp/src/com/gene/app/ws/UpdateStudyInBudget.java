85ca6d2d28bfe8dd5e33e320d546660a12b67f89
gayathri.307673 <budgetapptechm@users.noreply.github.com>
2015-06-10 12:30:47 +0000

    Delete web service changes.

--- BudgetApp/src/com/gene/app/ws/UpdateStudyInBudget.java
+++ BudgetApp/src/com/gene/app/ws/UpdateStudyInBudget.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,6 +34,8 @@ import com.google.gson.Gson;
 
 @Path("/updateProjectDetails")
 public class UpdateStudyInBudget {
+	private final static Logger LOGGER = Logger
+			.getLogger(UpdateStudyInBudget.class.getName());
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -38,31 +43,40 @@ public class UpdateStudyInBudget {
 			throws IOException {
 		ErrorObject eObj = new ErrorObject();
 		Gson gson = new Gson();
+		System.out.println("start time :"+System.currentTimeMillis());
 		ProjectParameters gMemori = gson.fromJson(gmem, ProjectParameters.class);
 		eObj= storeProjectData(gMemori, eObj);
 		String request = gson.toJson(eObj);
-		System.out.println("request :::::"+request);
+		System.out.println("end time :"+System.currentTimeMillis());
+		LOGGER.log(Level.INFO, "request :::::"+request);
 		return request;
 	}
 
 	public ErrorObject storeProjectData(ProjectParameters prjParam, ErrorObject eObj){
+		//Date date = new Date();
+		long starttime = System.currentTimeMillis();
+		LOGGER.log(Level.INFO, "Start Time: "+starttime);
 		DBUtil util = new DBUtil();
 		List<GtfReport> gtfReports = new ArrayList<GtfReport>();
 		List<String> selectedCC = new ArrayList<>();
 		GtfReport gtfReport = null;
 		List<String> costCenterList = prjParam.getCostCentres();
 		String pUnixId = prjParam.getProjectOwner();
+		
+		long userStartTime = System.currentTimeMillis();
 		UserRoleInfo userInfo = util.readUserRoleInfoByName(pUnixId);
-		System.out.println("userInfo"+userInfo.getUserName());
-		System.out.println("costCenterList"+costCenterList);
-		System.out.println("userInfo.getCostCenter() :::"+userInfo.getCostCenter());
-		System.out.println("userInfo null or empty"+(userInfo == null || !Util.isNullOrEmpty(userInfo.getEmail())));
+		long userEndTime = System.currentTimeMillis();
+		LOGGER.log(Level.INFO, "util.readUserRoleInfoByName(pUnixId) execution time"+(userEndTime-userStartTime));
+		LOGGER.log(Level.INFO, "userInfo"+userInfo.getUserName());
+		LOGGER.log(Level.INFO, "costCenterList"+costCenterList);
+		LOGGER.log(Level.INFO, "userInfo.getCostCenter() :::"+userInfo.getCostCenter());
+		LOGGER.log(Level.INFO, "userInfo null or empty"+(userInfo == null || !Util.isNullOrEmpty(userInfo.getEmail())));
 		if(userInfo == null || !Util.isNullOrEmpty(userInfo.getEmail())){
-			System.out.println("If loop userInfo null or empty"+(userInfo == null || !Util.isNullOrEmpty(userInfo.getEmail())));
+			LOGGER.log(Level.INFO, "If loop userInfo null or empty"+(userInfo == null || !Util.isNullOrEmpty(userInfo.getEmail())));
 			eObj.setStatusCode(401);
 			eObj.setStatusMessage("Authentication Failed !!!");
-			System.out.println("status Code"+eObj.getStatusCode());
-			System.out.println("status Message"+eObj.getStatusMessage());
+			LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+			LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 			return eObj;
 		}else{
 			for(String cCenter:costCenterList){
@@ -74,23 +88,26 @@ public class UpdateStudyInBudget {
 			if(selectedCC.isEmpty()){
 				eObj.setStatusCode(402);
 				eObj.setStatusMessage("User is not mapped to costCentres: " +costCenterList+" !!!");
-				System.out.println("status Code"+eObj.getStatusCode());
-				System.out.println("status Message"+eObj.getStatusMessage());
+				LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+				LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 				return eObj;
 			}
 		}
 		GtfReport gtfRpt = new GtfReport();
 		List<GtfReport>	gtfRptlst = new ArrayList<GtfReport>();
 		if(prjParam.getgMemoriId()!=null && !"".equals(prjParam.getgMemoriId())){
+			long GMemIdStartTime = System.currentTimeMillis();
 			gtfRptlst = util.readProjectDataByGMemId(prjParam.getgMemoriId());
+			long GMemIdEndTime = System.currentTimeMillis();
+			LOGGER.log(Level.INFO, "util.readProjectDataByGMemId(prjParam.getgMemoriId()) execution time"+(GMemIdEndTime-GMemIdStartTime));
 			if(gtfRptlst!=null && !gtfRptlst.isEmpty()){
 				gtfRpt = gtfRptlst.get(0);
 			}
 		}if( gtfRpt!=null && Util.isNullOrEmpty(gtfRpt.getgMemoryId()) && !selectedCC.contains(gtfRpt.getCostCenter())){
 			eObj.setStatusCode(404);
 			eObj.setStatusMessage("Project doesn't exist in costCenter " +costCenterList+" !!!");
-			System.out.println("status Code"+eObj.getStatusCode());
-			System.out.println("status Message"+eObj.getStatusMessage());
+			LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+			LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 			return eObj;
 		}
 		System.out.println("selectedCC::::"+selectedCC);
@@ -99,11 +116,12 @@ public class UpdateStudyInBudget {
 		String selectedCostCenter = "";
 		boolean isGMemIdExists = false; 
 		for(String cc: selectedCC){
+			long GMemIdStartTime = System.currentTimeMillis();
 			gtfRptMap = util.getAllReportDataFromCache(cc);
 			gMemoriId = prjParam.getgMemoriId();
-			System.out.println("selectedCC::::"+gtfRptMap);
-			System.out.println("gMemoriId::::"+gMemoriId);
-			System.out.println("gMemoriId.length()::::"+gMemoriId.length());
+			LOGGER.log(Level.INFO, "selectedCC::::"+gtfRptMap);
+			LOGGER.log(Level.INFO, "gMemoriId::::"+gMemoriId);
+			LOGGER.log(Level.INFO, "gMemoriId.length()::::"+gMemoriId.length());
 			if(gtfRptMap.get(gMemoriId)!=null && (gMemoriId.length()==6)){
 				isGMemIdExists = true;
 				selectedCostCenter = cc;
@@ -112,27 +130,27 @@ public class UpdateStudyInBudget {
 		}
 		//Map<String,GtfReport> gtfRptMap = util.getAllReportDataFromCache(costCenter);
 		//String gMemoriId = prjParam.getgMemoriId();
-		System.out.println("gMemoriId"+gMemoriId);
-		System.out.println("gtfRptMap.get(gMemoriId)"+gtfRptMap.get(gMemoriId));
+		LOGGER.log(Level.INFO, "gMemoriId"+gMemoriId);
+		LOGGER.log(Level.INFO, "gtfRptMap.get(gMemoriId)"+gtfRptMap.get(gMemoriId));
 		if(!isGMemIdExists){
 			createProjectInBudget(prjParam,costCenterList.get(0));
 			eObj.setStatusCode(200);
 			eObj.setStatusMessage("gMemori Id : " +gMemoriId+" has been created in Budgeting Tool !!!");
-			System.out.println("status Code"+eObj.getStatusCode());
-			System.out.println("status Message"+eObj.getStatusMessage());
+			LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+			LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 			return eObj;
 		}else{
-		System.out.println("gtfRptMap"+gtfRptMap);
+			LOGGER.log(Level.INFO, "gtfRptMap"+gtfRptMap);
 		for(Map.Entry<String, GtfReport> gtfEntry: gtfRptMap.entrySet()){
 			if(gtfEntry.getKey().contains(gMemoriId) && gtfEntry.getKey().length()<10){
 				gtfReport = gtfEntry.getValue();
-				System.out.println("prjParam.getProjectOwner() : "+prjParam.getProjectOwner() +"::::" +gtfReport.getRequestor());
+				LOGGER.log(Level.INFO, "prjParam.getProjectOwner() : "+prjParam.getProjectOwner() +"::::" +gtfReport.getRequestor());
 				if((gtfReport.getRequestor().contains(":") && !prjParam.getProjectOwner().equalsIgnoreCase(gtfReport.getRequestor().split(":")[1]) ) ||
 						(!gtfReport.getRequestor().contains(":") && !prjParam.getProjectOwner().equalsIgnoreCase(gtfReport.getRequestor()))){
 					eObj.setStatusCode(403);
 					eObj.setStatusMessage("User is not authorised to edit the project !!!");
-					System.out.println("status Code"+eObj.getStatusCode());
-					System.out.println("status Message"+eObj.getStatusMessage());
+					LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+					LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 					return eObj;
 				}
 				int flag = 0;
@@ -142,8 +160,8 @@ public class UpdateStudyInBudget {
 					if ((!Util.isNullOrEmpty(gtfReport.getPoNumber()))) {
 						eObj.setStatusCode(405);
 						eObj.setStatusMessage("Project cannot be closed in gMemori Budget as PO number is blank !!!");
-						System.out.println("status Code"+eObj.getStatusCode());
-						System.out.println("status Message"+eObj.getStatusMessage());
+						LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+						LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 						return eObj;
 					} else{
 					flag = 3;
@@ -151,7 +169,7 @@ public class UpdateStudyInBudget {
 					}
 				} else {
 					if ((!Util.isNullOrEmpty(gtfReport.getPoNumber()))) {
-						System.out.println("PO Number is blank. Hence status is set to New !!!");
+						LOGGER.log(Level.INFO, "PO Number is blank. Hence status is set to New !!!");
 						flag = 1;
 						status = BudgetConstants.status_New;
 					} else {
@@ -169,12 +187,15 @@ public class UpdateStudyInBudget {
 			}
 		}
 		}
-		System.out.println("status Code"+eObj.getStatusCode());
-		System.out.println("status Message"+eObj.getStatusMessage());
+		LOGGER.log(Level.INFO, "status Code"+eObj.getStatusCode());
+		LOGGER.log(Level.INFO, "status Message"+eObj.getStatusMessage());
 		util.generateProjectIdUsingJDOTxn(gtfReports);
 		util.storeProjectsToCache(gtfReports, selectedCostCenter, BudgetConstants.NEW);
 		eObj.setStatusCode(200);
 		eObj.setStatusMessage("Successful !!!");
+		//date = new Date();
+		//long t2 = date.getTime();
+		//LOGGER.log(Level.INFO, "Time taken : "+(t2-t1));
 		return eObj;
 	}
 
