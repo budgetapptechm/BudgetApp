7c0bf442589fd3fd5dd1e1fb6065a84981f34c44
gayathri.307673@gmail.com <gayathri.307673@gmail.com@80e4d81a-3405-206f-6867-bf47240bd553>
2015-07-14 12:40:07 +0000

    rounding off fix

--- trunk/BudgetApp/src/com/gene/app/action/FactSheetUploadServlet.java
+++ trunk/BudgetApp/src/com/gene/app/action/FactSheetUploadServlet.java
@@ -409,7 +409,7 @@ public class FactSheetUploadServlet extends HttpServlet {
 		    		try{
 					if (gtfRpt.getgMemoryId().contains(".")) {
 						gtfRpt.setPercent_Allocation(Util.roundDoubleValue((gtfRpt.getPlannedMap()
-								.get("TOTAL") / total) * 100 , 5));
+								.get("TOTAL") / total) * 100 , 2));
 					}}catch(NumberFormatException nfe){
 						gtfRpt.setPercent_Allocation(100.0);
 					}catch(ArithmeticException ae){
